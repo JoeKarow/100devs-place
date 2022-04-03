2e730d655a408fbca6cd8716dd6db9e372d05a82
Greg Ley <greg.ley@gmail.com>
2022-04-03 00:01:55 -0400

    new image source

--- 100-devs-overlay.js
+++ 100-devs-overlay.js
@@ -13,7 +13,7 @@ if (window.top !== window.self) {
         document.getElementsByTagName("mona-lisa-embed")[0].shadowRoot.children[0].getElementsByTagName("mona-lisa-canvas")[0].shadowRoot.children[0].appendChild(
             (function () {
                 const i = document.createElement("img");
-                i.src = "https://cosmic-starburst-109ff7.netlify.app/img/100-devs-place-overlay.png";
+                i.src = "https://raw.githubusercontent.com/Necroni/100devs-place/main/100-devs-place-overlay.png";
                 i.onload = () => {
                     if (i.width === i.height) {
                         i.style = "position: absolute;left: 0;top: 0;image-rendering: pixelated;width: 1000px;height: 1000px;";
