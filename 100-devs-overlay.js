41303d96d7eec36a46c6dff0de222989cc81c9f4
Greg Ley <greg.ley@gmail.com>
2022-04-02 23:33:00 -0400

    yeehaw

---
+++ 100-devs-overlay.js
@@ -0,0 +1,27 @@
+// ==UserScript==
+// @name         100Devs logo template
+// @namespace    http://tampermonkey.net/
+// @version      0.4
+// @description  Peace and harmony with allies
+// @author       oralekin - borrowed by aaaarchy
+// @match        https://hot-potato.reddit.com/embed*
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=reddit.com
+// @grant        none
+// ==/UserScript==
+if (window.top !== window.self) {
+    window.addEventListener('load', () => {
+        document.getElementsByTagName("mona-lisa-embed")[0].shadowRoot.children[0].getElementsByTagName("mona-lisa-canvas")[0].shadowRoot.children[0].appendChild(
+            (function () {
+                const i = document.createElement("img");
+                i.src = "https://cosmic-starburst-109ff7.netlify.app/img/100-devs-place-overlay.png";
+                i.onload = () => {
+                    if (i.width === i.height) {
+                        i.style = "position: absolute;left: 0;top: 0;image-rendering: pixelated;width: 1000px;height: 1000px;";
+                    } else {
+                        i.style = "position: absolute;left: 0;top: 0;image-rendering: pixelated;width: 2000px;height: 1000px;";
+                    }
+                };
+                return i;
+            })())
+    }, false);
+}
---
+++ readme.md
@@ -0,0 +1,27 @@
+# 100Devs Overlay for r/place
+
+## Note: this involves installing extensions that can run scripts in your browser. Always be careful with these.
+
+## Steps:
+
+1. Install the relevant extension for your browser
+
+Chrome: TamperMonkey
+
+Firefox: ViolentMonkey
+
+Not available on mobile devices!
+
+2. Install the script
+
+100-devs-overlay.js
+
+## Updates:
+
+I'm just one person but I'll do my best. Feel free to clone and redirect folks if needed.
+
+## Credits:
+
+HUGE kudos to r/osuplace for the original code: oralekin & exdeejay
+
+AND to the r/greenlattice team, who I copied directly! Nice work artillect#8709, jcb#1032 & jumpinglizard#4404
